2548c42a9aeae17e033f84b75d8e8f9921f6ba34
JBrancale <james.brancale@gmail.com>
2017-12-08 15:38:29 -0500

    Add files via upload

--- Server.java
+++ Server.java
@@ -1,4 +1,3 @@
-
 /*
  * Written by:
  * James Brancale
@@ -7,55 +6,86 @@
  */
 
 import java.sql.*;
-
-public class Server {
-	private Connection con;
-	private Statement stmt;
-
-	public Statement connect() {
-		
-		try {
-			
-			// URL Syntax: jdbc:TYPE:machine:port/DB_NAME
-			// port omitted in this example
-
-			// for local host
-			// String URL = "jdbc:mysql://localhost.njit.edu/UCID";
-
-			// for server
-			String UCID = "";
-			String passwd = "";
-			String URL = "jdbc:mysql://sql2.njit.edu/" + UCID;
-			//
-			// establishing connection
-			// supply URL, user, password
-			//
-
-			con = DriverManager.getConnection(URL, UCID, passwd);
-			stmt = con.createStatement();
-			
-			// catch database exceptions and print info
-		} catch (SQLException e) {
-			System.err.println(" SQL Exceptions \n");
-			while (e != null) {
-				System.out.println("Error Description: " + e.getMessage());
-				System.out.println("SQL State:  " + e.getSQLState());
-				System.out.println("Vendor Error Code: " + e.getErrorCode());
-				e = e.getNextException();
-				System.out.println("");
-			}
-		}
-		return null;
-	}
-	public void removeByID(int docId) {
-		
-		String query = "DELETE FROM READER WHERE READERID = '" + docId +"';";
-		
-		try {
-			stmt.executeQuery(query);
-		} catch (SQLException e) {
-			new popupMsg();
-		}
-	}
+class Server {
+	private static Connection con;
+    private static Statement stmt;
 	
+    public static void main(String[] args) {
+    	removeByID(1);
+    }
+    
+    private static void connectToDB() {
+        System.out.println("Hello from MySQL!\n");
+
+        //loading driver, need to catch exceptions
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch(java.lang.ClassNotFoundException e) {
+            System.err.println("ClassNotFoundException: ");
+            System.err.println("Message: " + e.getMessage());
+        }
+
+        try {
+                // URL Syntax: jdbc:TYPE:machine:port/DB_NAME
+                // port omitted in this example
+
+                // for local host
+                // String URL = "jdbc:mysql://localhost.njit.edu/UCID";
+
+                // for server
+                String UCID = "";
+                String passwd = "";
+                String URL = "jdbc:mysql://sql2.njit.edu/" + UCID;
+                //
+                // establishing connection 
+                // supply URL, user, password
+                //
+
+                con = DriverManager.getConnection(URL, UCID, passwd);
+                stmt = con.createStatement();
+
+
+        // catch database exceptions and print info
+        } catch(SQLException e) {
+        	System.err.println(" SQL Exceptions \n");
+            while (e != null) {
+                System.out.println("Error Description: " + e.getMessage());
+                System.out.println("SQL State:  " + e.getSQLState());
+                System.out.println("Vendor Error Code: " + e.getErrorCode());
+                e = e.getNextException();
+                System.out.println("");
+            }
+        }
+    }
+    
+    public static void removeByID(int docId) {
+    	
+    	connectToDB();
+		
+		try {
+			// AUTHOR
+            System.out.println("AUTHOR");
+            String query = "SELECT * FROM AUTHOR;";
+            ResultSet rs = stmt.executeQuery(query);
+            System.out.println("AUTHORID\tANAME");
+            while (rs.next()) {
+                int s = rs.getInt("AUTHORID");
+                String n = rs.getString("ANAME");
+                System.out.println(s + "\t" + n);
+            }
+            //close resources
+            stmt.close(); con.close();
+		} catch (SQLException e) {
+			//new popupMsg();
+			System.err.println(" SQL Exceptions \n");
+            while (e != null) {
+                System.out.println("Error Description: " + e.getMessage());
+                System.out.println("SQL State:  " + e.getSQLState());
+                System.out.println("Vendor Error Code: " + e.getErrorCode());
+                e = e.getNextException();
+                System.out.println("");
+            }
+		}
+	}
 }
+
